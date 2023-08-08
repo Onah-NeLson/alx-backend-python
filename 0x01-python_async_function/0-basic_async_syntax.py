9c991535ce901b7b3f55dc757a10a6ef7ade5c2f
Onah-NeLson <Onahneslon2@gmail.com>
2023-08-08 04:48:56 +0000

    async_function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-"""The intent of this script, is to Write an asynchronous coroutine that takes in an integer argument
-(max_delay, with a default value of 10) named wait_random that waits
-for a random delay between 0 and max_delay (included and float value)
+"""
+The intent of this script, is to Write an asynchronous coroutine
+that takes in an integer argument(max_delay, with a default value of 10)
+named wait_random that waitsfor a random delay between 0 and max_delay
+(included and float value)
 seconds and eventually returns it.
-
 Use the random module.
+
 """
 
 
