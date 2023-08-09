d2e7269bb0ec72b835755035980f3eade635c44a
Onah-NeLson <Onahneslon2@gmail.com>
2023-08-09 04:07:01 +0000

    Async_comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,8 +2,9 @@
 """
 Script Hint: a coroutine called async_generator that takes no arguments.
 
-The coroutine will loop 10 times, each time asynchronously wait 1 second, then yield a random number between 0 and 10. Using the random module.
-
+The coroutine will loop 10 times, each time asynchronously
+wait 1 second, then yield a random number
+between 0 and 10. Using the random module.
 """
 
 from asyncio import sleep
